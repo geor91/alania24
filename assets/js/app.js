9c90d3c7eaf0fafd868844e9bb6a850405166625
JohnyM <jorjiko91@gmail.com>
2016-01-18 19:19:56 +0300

    gallery page and some fixes

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	// init slider on gallery page
+	$('#sliderID').owlCarousel({
+		pagination: false,
+		navigation: true,
+		navigationText: false,
+		slideSpeed: 400,
+		singleItem:true
+	});
+});
