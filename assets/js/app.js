ea4491a5d6e4e31072e4dd4efd0a7187a9e3aad8
Johny Martin <jorjiko91@gmail.com>
2016-01-25 18:13:14 +0300

    some fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,4 +7,15 @@ $(document).ready(function() {
 		slideSpeed: 400,
 		singleItem:true
 	});
+
+	// init slider on gallery page
+	$('#hot-news-slider').owlCarousel({
+		pagination: false,
+		navigation: false,
+		slideSpeed: 1000,
+		autoPlay: true,
+		mouseDrag: false,
+		touchDrag: false,
+		singleItem:true
+	});
 });
