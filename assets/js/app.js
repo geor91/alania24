c639df275aef7247df4abf3461b17876d5b40c72
JohnyM <jorjiko91@gmail.com>
2016-03-05 21:19:38 +0300

    new mobile styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 		navText: false
 	});
 
-	// init slider on gallery page
+	// init slider on index page (header)
 	$('#hot-news-slider').owlCarousel({
 		autoplay: true,
 		autoplayHoverPause: true,
@@ -25,10 +25,36 @@ $(document).ready(function() {
 });
 
 // EVENTS
-	// переключение между формами авторизации, регистрации
+	// switching between auth, reg forms
 		$('.auth-area a.trigger').on('click', function(e) {
 			var divID = $(this).attr('href');
 			$(this).parents('.auth-area').find('.auth-form').removeClass('active');
 			$(divID).addClass('active');
 			e.preventDefault();
+		});
+
+	// navbar expander on mobile devices
+		$('.navbar-expander').on('click', function(e) {
+			var $navbar = $('#main-container'),
+				$overlay = $('.navbar-overlay');
+
+			if (!$navbar.hasClass('expanded')) {
+				$('body').addClass('no-scroll');
+
+				// раскрываем целевой блок в зависимости от режима
+				$navbar.addClass('expanded');
+				$overlay.show();
+			}
+			else {
+				$navbar.removeClass('expanded');
+				$overlay.hide();
+				$('body').removeClass('no-scroll');
+			}
+			e.preventDefault();
+		});
+
+		$('.navbar-overlay').click( function(e) {
+			$('#main-container').removeClass('expanded');
+			$('.navbar-overlay').hide();
+			$('body').removeClass('no-scroll');
 		});
