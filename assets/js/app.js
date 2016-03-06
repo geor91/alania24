10cc344fece8984be3ae6a4763a46c8b8dc15470
JohnyM <jorjiko91@gmail.com>
2016-03-06 20:13:24 +0300

    social share buttons, some fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,6 +32,7 @@ $(document).ready(function() {
 			$(divID).addClass('active');
 			e.preventDefault();
 		});
+	// --------------------------------
 
 	// navbar expander on mobile devices
 		$('.navbar-expander').on('click', function(e) {
@@ -58,3 +59,53 @@ $(document).ready(function() {
 			$('.navbar-overlay').hide();
 			$('body').removeClass('no-scroll');
 		});
+	// ----------------------------------
+
+	// Social Share Buttons
+		$('[data-social]').click(function(e) {
+			var url		= document.URL,
+				title	= $('.post-title').text(),
+				text	= $('.post-excerpt').text(),
+				image	= 'http://alania24.ru' + $('[data-social_img]:first').data('social_img'),
+				query	= '';
+
+			if ($(this).data('social') == 'vk') {
+				query =  'http://vkontakte.ru/share.php?';
+				query += 'url='          + encodeURIComponent(url);
+				query += '&title='       + encodeURIComponent(title);
+				query += '&description=' + encodeURIComponent(text);
+				query += '&image='       + encodeURIComponent(image);
+				query += '&noparse=true';
+			} 
+			else if ($(this).data('social') == 'fb') {
+				query = 'http://www.facebook.com/sharer.php?s=100';
+				query+= '&p[title]='     + encodeURIComponent(title);
+				query+= '&p[summary]='   + encodeURIComponent(text);
+				query+= '&p[url]='       + encodeURIComponent(url);
+				query+= '&p[images][0]=' + encodeURIComponent(image);
+			}
+			else if ($(this).data('social') == 'tw') {
+				query = 'http://twitter.com/share?';
+				query+= 'text='      + encodeURIComponent(title);
+				query+= '&url='      + encodeURIComponent(url);
+				query+= '&counturl=' + encodeURIComponent(url);
+			}
+			else if ($(this).data('social') == 'ok') {
+				query = 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1';
+				query+= '&st.comments=' + encodeURIComponent(text);
+				query+= '&st._surl='    + encodeURIComponent(url);
+			}
+			else if ($(this).data('social') == 'mlru') {
+				query = 'http://connect.mail.ru/share?';
+				query+= 'url='          + encodeURIComponent(url);
+				query+= '&title='       + encodeURIComponent(title);
+				query+= '&description=' + encodeURIComponent(text);
+				query+= '&imageurl='    + encodeURIComponent(image);
+			}
+
+			if (true) {}
+			window.open(query,'','toolbar=0,status=0,width=640,height=480');
+
+			e.preventDefault();
+		});
+	// ------
